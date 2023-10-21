76694b0b2448e89c67562e44045a014476a42f8d
Aleak1ra <alexandre_bernardi95@hotmail.com>
2023-10-20 22:42:31 -0300

    chore: build files for gh page

--- src/components/TaskDetails.jsx
+++ src/components/TaskDetails.jsx
@@ -15,12 +15,13 @@ const TaskDetails = () => {
 
   return (
     <>
-      <div className="back-button-container">
-        <Button onClick={handleBackButtonClick}>Voltar</Button> 
-        </div>
+      
         <div className="task-details-container">
           <h2>{params.taskTitle}</h2>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, sit non consequatur illo perspiciatis earum.</p>
+        </div>
+        <div className="back-button-container">
+        <Button onClick={handleBackButtonClick}>Voltar</Button> 
         </div>
       
     </>
